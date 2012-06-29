437d1e64fcf15892c3584669d021be5904e2314a
Rich Neswold <neswold@fnal.gov>
2012-06-29 16:35:20 -0500

    Add on/off control for the state devices. Ignore first setting (assume it's from download service.)

--- states_dev.erl
+++ states_dev.erl
@@ -18,7 +18,7 @@
 
 -import(error_logger, [info_msg/1, info_msg/2, warning_msg/2]).
 
--define(ATOMIC_SIZE,2).
+-define(ATOMIC_SIZE, 2).
 
 -record(mystate, {table=ets:new(stateDevTable, [set, private]),
 		  di_alive=undefined :: undefined | integer(),
@@ -27,17 +27,40 @@
 		  socket,
 		  seq=0}).
 
+update_status(Tid, DI, State) ->
+    case ets:lookup(Tid, DI) of
+	[] -> ok;
+	[{_, Val, _}] ->
+	    ets:insert(Tid, {DI, Val, State})
+    end.
+
+update_value(Tid, DI, Val) ->
+    case ets:lookup(Tid, DI) of
+	[] ->
+	    ets:insert(Tid, {DI, Val, true}),
+	    false;
+
+	[{_, _, State}] ->
+	    ets:insert(Tid, {DI, Val, State}),
+	    State
+    end.
+
 %%% Multicast a STATES protocol message.
 
 report_new_state(#mystate{table=Tid, seq=Seq} = S, DI, Val) ->
     {MSec, Sec, USec} = os:timestamp(),
-    ets:insert(Tid, {DI, Val}),
-    Data = [<<1:16/little, Seq:16/little, 1:32/little>>,
-	    <<0:16/little, DI:32/little, Val:16/little>>,
-	    <<(MSec * 1000000 + Sec):32/little,
-	      (USec * 1000):32/little>>],
-    acnet:send_usm(state, "STATES@STATES", Data),
-    S#mystate{seq=(Seq + 1) band 16#ffff}.
+    case update_value(Tid, DI, Val) of
+	true ->
+	    Data = [<<1:16/little, Seq:16/little, 1:32/little>>,
+		    <<0:16/little, DI:32/little, Val:16/little>>,
+		    <<(MSec * 1000000 + Sec):32/little,
+		      (USec * 1000):32/little>>],
+	    acnet:send_usm(state, "STATES@STATES", Data),
+	    S#mystate{seq=(Seq + 1) band 16#ffff};
+
+	false ->
+	    S
+    end.
 
 %%% Calculate the delay needed to wait until 5 seconds after the last
 %%% timeout. If the system doesn't have a DI defined, then we wait
@@ -73,10 +96,20 @@ loop(#mystate{table=Tid, last_alive=Last, count_alive=Count} = S) ->
 
 	     { CPid, read, DI } ->
 		 case ets:lookup(Tid, DI) of
-		     [] ->
-			 CPid ! {error, not_found};
-		     [{_, Val}] ->
-			 CPid ! {ok, Val}
+		     [] -> CPid ! {error, not_found};
+		     [{_, Val, _}] -> CPid ! {ok, Val}
+		 end,
+		 S;
+
+	     { CPid, enable, State, DI } ->
+		 CPid ! ok,
+		 update_status(Tid, DI, State),
+		 S;
+
+	     { CPid, status, DI } ->
+		 case ets:lookup(Tid, DI) of
+		     [] -> CPid ! {ok, false};
+		     [{_, _, Val}] -> CPid ! {ok, Val}
 		 end,
 		 S;
 
@@ -176,7 +209,7 @@ set_states(#setdat_1device{di=DI, settingdata= <<Val:16/little>>,
 	   #aux_spec{shared=Pid}) ->
     set_dev(Pid, DI, Val, Min, Max).
 
-read_states(#device_request{di=DI}, #aux_spec{shared=Pid,trigger=SE}) ->
+read_states(#device_request{di=DI}, #aux_spec{shared=Pid, trigger=SE}) ->
     Pid ! { self(), read, DI },
     receive
 	{ok, Val} ->
@@ -189,15 +222,44 @@ read_states(#device_request{di=DI}, #aux_spec{shared=Pid,trigger=SE}) ->
 			  data = <<0,0>>}
     after 100 ->
 	    #device_reply{status=?ERR_READTMO,
+			  stamp = os:timestamp(),
+			  data = <<0,0>>}
+    end.
+
+set_status(#setdat_1device{di=DI, settingdata= <<Val:16/little>>},
+	   #aux_spec{shared=Pid}) ->
+    Pid ! { self(), enable, Val =:= 1, DI },
+    receive
+	ok -> ?ACNET_SUCCESS
+    after 100 -> ?ERR_READTMO
+    end.
+
+bool_to_int(true) -> 1;
+bool_to_int(false) -> 0.
+
+read_status(#device_request{di=DI}, #aux_spec{shared=Pid, trigger=SE}) ->
+    Pid ! { self(), status, DI },
+    receive
+	{ok, Val} ->
+	    #device_reply{status=?ACNET_SUCCESS,
 			  stamp = SE#sync_event.stamp,
+			  data = <<(bool_to_int(Val)):16/little>>}
+    after 100 ->
+	    #device_reply{status=?ERR_READTMO,
+			  stamp = os:timestamp(),
 			  data = <<0,0>>}
     end.
 
 start(Oid) ->
     Pid = spawn_link(states_dev, init_task,[]),
     spawn_link(states_dev, init_fsmset, [Pid]),
-    SpecA = #device_spec{ readf=fun read_states/2, setf=fun set_states/2,
+    Spec1 = #device_spec{ readf=fun read_states/2, setf=fun set_states/2,
 			  atomic_bytes=?ATOMIC_SIZE, max_elements=1,
 			  shared=Pid },
-    devices:install_pi_attr_functions(Oid, ?PI_READNG, 0, SpecA),
-    devices:install_pi_attr_functions(Oid, ?PI_SETTNG, 0, SpecA).
+    Spec2 = #device_spec{ readf=fun read_status/2, setf=fun set_status/2,
+			  atomic_bytes=?ATOMIC_SIZE, max_elements=1,
+			  shared=Pid },
+    devices:install_pi_attr_functions(Oid, ?PI_READNG, 0, Spec1),
+    devices:install_pi_attr_functions(Oid, ?PI_SETTNG, 0, Spec1),
+    devices:install_pi_attr_functions(Oid, ?PI_BASTAT, 0, Spec2),
+    devices:install_pi_attr_functions(Oid, ?PI_BCNTRL, 0, Spec2).
