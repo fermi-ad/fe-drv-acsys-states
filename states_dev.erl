9bf9fcb923e94f5af10922100eeeb6844b2edfae
Rich Neswold <neswold@fnal.gov>
2012-09-28 13:41:13 -0500

    The report states function updates the value, too.

--- states_dev.erl
+++ states_dev.erl
@@ -182,10 +182,10 @@ message(S, #acnet_request{ref=RpyId, mult=true} = Req) ->
     acnet:send_last_reply(RpyId, ?ACNET_BADREQ, <<>>),
     S;
 message(#mystate{table=Tid} = S, {timeout, _, DI}) ->
+    info_msg("Transmitting ALIVE state.~n"),
     erlang:start_timer(5000, self(), DI),
     {_, Count} = read_value(Tid, DI),
-    update_value(Tid, DI, (Count + 1) band 16#ffff),
-    report_new_state(S, DI, Count);
+    report_new_state(S, DI, (Count + 1) band 16#ffff);
 message(S, #acnet_request{ref=RpyId} = Req) ->
     info_msg("FSMSET Unhandled request: ~p.~n", [Req]),
     acnet:send_last_reply(RpyId, ?ACNET_SYS, <<>>),
