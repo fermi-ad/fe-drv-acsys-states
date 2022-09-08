b88df4ad47f07fdd11f2533405f912f293536ecf
Rich Neswold <neswold@fnal.gov>
2022-09-08 13:57:54 -0500

    :pencil2: add comments

--- states_dev.erl
+++ states_dev.erl
@@ -108,6 +108,8 @@ devs(Bin) ->
 %%% ===========================================================================
 %%% This section defines the 'driver' behavior interface.
 
+%%% Initialize the driver's state and the resources it uses.
+
 -spec init(any()) ->
 		  {'ready', #mystate{}, array:array()} |
 		  {'error', string()}.
@@ -115,19 +117,39 @@ devs(Bin) ->
 init(_) ->
     {'ok', S} = gen_udp:open(0),
     try
+
+	%% Create the FSMSET handle and enable it for receiving
+	%% requests.
+
 	acnet:start('fsmset', "FSMSET", "STATE"),
 	acnet:accept_requests('fsmset'),
+
+	%% Create the table that will hold the state of the state
+	%% devices.
+
 	Tid = ets:new('stateDevTable', ['set', 'private']),
+
+	%% Build the ACSys/FE framework attribute list. This driver
+	%% has only two attributes for its devices.
+
 	Attrs = [#attr_spec{typ_elem='Int16', num_elem=1, name='state',
 			    description= <<"Read/set a state device.">>},
 		 #attr_spec{typ_elem='Int16', num_elem=1, name='status',
 			    description= <<"Read/set a state device's status.">>}],
+
+	%% If the configuration defined a "keep alive" state device,
+	%% add it to the table (with an initial value of 0) and start
+	%% a 5 second, periodic timer to update it.
+
 	case get_alive_di() of
 	    'undefined' -> 'ok';
 	    DI ->
 		update_value(Tid, DI, 0),
 		timer:send_interval(5000, {'timeout', DI})
 	end,
+
+	%% Return a success result to the framework.
+
 	{'ready', #mystate{socket=S, table=Tid}, array:from_list(Attrs)}
     catch
 	_:_ ->
@@ -135,13 +157,31 @@ init(_) ->
 	    {'error', "Error connecting to ACNET."}
     end.
 
+%%% Local helper function to conevrt a boolean to an integer (Erlang
+%%% doesn't define this conversion in the language.)
+
 bool_to_int('true') -> 1;
 bool_to_int('false') -> 0.
 
+%%% This function is called by the framework when reading the `state`
+%%% attribute.
+
 reading(S, _, #reading_context{attribute='state', di=DI},
 	#sync_event{stamp=Stamp}) ->
+
+    %% Retrieve the value associated with the given device index along
+    %% with the result status of the look-up.
+
     {Status, Data} = read_value(S#mystate.table, DI),
+
+    %% Return our state (it wasn't updated) along with the reply
+    %% record for the request.
+
     {S, #device_reply{stamp=Stamp, data= <<Data:16/little>>, status=Status}};
+
+%%% This function is executed when the framework wants to read the
+%%% `status` attribute.
+
 reading(S, _, #reading_context{attribute='status', di=DI},
 	#sync_event{stamp=Stamp}) ->
     {S, #device_reply{stamp=Stamp, status=?ACNET_SUCCESS,
@@ -152,10 +192,21 @@ reading(S, _, #reading_context{attribute='status', di=DI},
 				   <<(bool_to_int(Val)):16/little>>
 			   end}}.
 
+%%% This function is called when handling setting requests in the
+%%% framework for the `state` attribute.
+
 setting(S, #setting_context{ssdn= <<_:32, Mn:16/little, Mx:16/little>>, di=DI,
 			    attribute='state'}, <<V:16/little>>) ->
     set_dev(S, DI, V, Mn, Mx);
+
+%%% This function is called when handling setting requests in the
+%%% framework for the `status` attribute.
+
 setting(S, #setting_context{attribute='status', di=DI}, <<Val:16/little>>) ->
+
+    %% Update the enable/disable state of the device. If the settings
+    %% is 2, the device gets enabled. Otherwise it's disabled.
+
     update_status(S#mystate.table, DI, Val =:= 2),
     {S, ?ACNET_SUCCESS}.
 
@@ -175,13 +226,24 @@ message(S, #acnet_request{data= <<10:16/little, Count:16/little, _Some:48,
 			{NAcc, _} = set_dev(Acc, DI, V, -16#8000, 16#7fff),
 			NAcc
 		end, S, devs(Rest));
+
+%%% No FSMSET request needs multiple replies. Return a BAD REQUEST
+%%% status.
+
 message(S, #acnet_request{ref=RpyId, mult='true'} = Req) ->
     info_msg("FSMSET Bad request: ~p.~n", [Req]),
     acnet:send_last_reply(RpyId, ?ACNET_BADREQ, <<>>),
     S;
+
+%%% If we get a timeout message, it's time to send out the "keep
+%%% alive" state.
+
 message(#mystate{table=Tid} = S, {'timeout', DI}) ->
     {_, Count} = read_value(Tid, DI),
     report_new_state(S, DI, (Count + 1) band 16#ffff);
+
+%%% Report any other type of request.
+
 message(S, #acnet_request{ref=RpyId} = Req) ->
     info_msg("FSMSET Unhandled request: ~p.~n", [Req]),
     acnet:send_last_reply(RpyId, ?ACNET_SYS, <<>>),
